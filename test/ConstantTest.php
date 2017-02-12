f401208fc32b2856ecf21b666cc221c847019f78
nicolaasuni <info@tecnick.com>
2017-02-12 13:41:39 +0000

    Update dependencies and tests

--- test/ConstantTest.php
+++ test/ConstantTest.php
@@ -15,6 +15,8 @@
 
 namespace Test;
 
+use PHPUnit\Framework\TestCase;
+
 /**
  * Constant Test
  *
@@ -26,7 +28,7 @@ namespace Test;
  * @license     http://www.gnu.org/copyleft/lesser.html GNU-LGPL v3 (see LICENSE.TXT)
  * @link        https://github.com/tecnickcom/tc-lib-unicode-data
  */
-class ConstantTest extends \PHPUnit_Framework_TestCase
+class ConstantTest extends TestCase
 {
     public function testConstants()
     {
